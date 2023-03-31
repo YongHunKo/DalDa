cf81208b4b731771f31b89432ab1e2ae8196059b
YongHunKo <dydgnsrh5460@naver.com>
2023-04-01 00:46:29 +0900

    setting

---
+++ src/main/java/com/test/DaldaApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaldaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaldaApplication.class, args);
+	}
+
+}
